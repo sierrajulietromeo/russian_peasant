127f0c7a96ea124d8649542842bfe56fbb542ed9
Steve Rich <steven.john.rich@gmail.com>
2020-01-14 08:21:12 +0000

    Added validation for integers and improved formatting and readability.

--- main.py
+++ main.py
@@ -2,12 +2,27 @@ numberList1 = []
 numberList2 = []
 grandTotal = 0
 
-number1 = int(input("Enter number 1: "))
-number2 = int(input("Enter number 2: "))
+
+def inputNumber(message):
+  while True:
+    try:
+       userInput = int(input(message))       
+    except ValueError:
+       print("Not an integer! Try again.")
+       continue
+    else:
+       return userInput 
+       break 
+
+
+print("The Russian peasant algorithm")
+print()
+
+number1 = inputNumber("Enter number 1: ")
+number2 = inputNumber("Enter number 2: ")
 
 numberList1.append(number1)
 numberList2.append(number2)
-
 
 while number1 != 1:
 
@@ -22,8 +37,5 @@ for i in range(len(numberList1)):
     grandTotal = grandTotal + numberList2[i]
     print("{} (plus {}) ".format(grandTotal, numberList2[i]))
 
-    
+print()    
 print(numberList1[0], "multiplied by", numberList2[0], "is", grandTotal)
-
-
-
